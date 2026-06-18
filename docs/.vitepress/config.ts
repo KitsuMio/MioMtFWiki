6c061cd0d6ed41aa376083e156b1617b70f491c3
KitsuMio <KitsuMio@outlook.com>
2026-06-18 13:48:24 +0800

    Initial commit

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,31 @@
+import { defineConfig } from 'vitepress'
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: "MioMtFWiki",
+  description: "来自社区、服务社区的现代跨性别知识库",
+  base: '/MioMtFWiki/',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: '首页', link: '/' },
+      { text: 'HRT入门', link: '/hrt-start' }
+    ],
+
+    sidebar: [
+      {
+        text: '目录',
+        items: [
+          { text: '首页', link: '/' },
+          { text: '我怀疑自己是MtF，该做什么？', link: '/first-step' },
+          { text: '概念与术语', link: '/concepts' },
+          { text: 'HRT入门', link: '/hrt-start' }
+        ]
+      }
+    ],
+
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/KitsuMio/MioMtFWiki' }
+    ]
+  }
+})
---
+++ docs/.vitepress/theme/index.ts
@@ -0,0 +1,10 @@
+import DefaultTheme from 'vitepress/theme'
+
+import './fonts.css'
+import './colors.css'
+import './layout.css'
+import './components.css'
+import './animation.css'
+import './dark.css'
+
+export default DefaultTheme
